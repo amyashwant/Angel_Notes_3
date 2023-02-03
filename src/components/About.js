fa8ac9a5daaa2208b53764c824bef75f997a3b74
amyashwant <poonamgangwar083@gmail.com>
2023-02-03 17:10:04 +0530

    1st

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const About = () => {
+    return (
+        <div style={{marginLeft:"40%"}}>
+            MANAGE YOUR PERSONAL NOTES
+        </div>
+    )
+}
+
+export default About;
